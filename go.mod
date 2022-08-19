e3cb7bb9bcf99226d11e70b48e63d1ad46dd2106
yogipristiawan <yogistanada252@gmail.com>
2022-08-20 00:52:59 +0700

    feat: hello-world

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yogipristiawan/golang-mq
+
+go 1.19
+
+require github.com/rabbitmq/amqp091-go v1.4.0 // indirect
---
+++ hello-world/producer/producer.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"greetings", // name,
+		false,       // durable
+		false,       // delete when unused
+		false,       // exclusive
+		false,       // no-wait
+		nil,         // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	// take arguments from command line
+	msg := strings.Join(os.Args[1:], " ")
+
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(msg),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	log.Printf("[x] Sent %s", msg)
+}
